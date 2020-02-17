3294c68347854bdcce61e0cfe5d2148e537b996b
Jack Goodwin <jackgoodwin15@gmail.com>
2020-02-17 16:53:42 +0000

    Implemented secure api key

--- src/main/java/com/sparta/engineering50/App.java
+++ src/main/java/com/sparta/engineering50/App.java
@@ -1,6 +1,6 @@
 package com.sparta.engineering50;
 
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
--- src/main/java/com/sparta/engineering50/Mapper.java
+++ src/main/java/com/sparta/engineering50/Mapper.java
@@ -3,7 +3,9 @@ package com.sparta.engineering50;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.Properties;
 
 public class Mapper {
     ObjectMapper objectMapper;
@@ -14,8 +16,9 @@ public class Mapper {
     }
 
     public void readJsonToObject() {
+        String key = getApiKey();
         try {
-            mapPOJO = objectMapper.readValue(new URL("http://api.openweathermap.org/data/2.5/weather?q=London&appid=79b94cd487024d6faf17794f7e7cab25"), MapPOJO.class);
+            mapPOJO = objectMapper.readValue(new URL("http://api.openweathermap.org/data/2.5/weather?q=London&appid=" + key), MapPOJO.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -23,5 +26,17 @@ public class Mapper {
 
     public MapPOJO getMapPOJO() {
         return mapPOJO;
+    }
+
+    public String getApiKey() {
+        Properties prop = new Properties();
+
+        try (InputStream input = Mapper.class.getClassLoader().getResourceAsStream("config.properties")) {
+            prop.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return prop.getProperty("API_KEY");
     }
 }
